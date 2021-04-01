00fa49a2c1117bf345d592a5e0841dc5e6b98609
AlexeyFedenev <onetouch888@yandex.ru>
2021-04-01 13:26:52 +0300

    Задача17

--- TaskWork17.java
+++ TaskWork17.java
@@ -12,21 +12,18 @@ public class TaskWork17 {
             System.out.println("Введите строку " + (i + 1));
             stringArr[i] = in.next();
         }
-        int cont = 0;
-        char ch = 0;
-        char ch1 = 0;
-        for (int i = 0; i < lengthArr; i++) {
-            for (int x = 0; x < stringArr[i].length(); x++) {
-                for (int c = x + 1; c < stringArr[i].length(); c++) {
-                    if (stringArr[i].charAt(x) == stringArr[i].charAt(c)) {
-                        ch =stringArr[i].charAt(c);
-                        System.out.println(ch);
-                    }
-                }
+        int maxLength = 0;
+        int maxStrIndex = 0;
+        for (int i = 0; i < stringArr.length; i++) {
+            int currentLength = (int) stringArr[i].chars().distinct().count();
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+                maxStrIndex = i;
             }
-            System.out.println(ch);
         }
+        System.out.println(stringArr[maxStrIndex]);
     }
 }
+
 
 
